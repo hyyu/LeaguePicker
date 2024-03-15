fb8204af3dcec14912b358a1bc48d1e1633cafba
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-15 17:24:00 +0100

    Module for main page

---
+++ ui/home/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlin.android.plugin)
+}
+
+android {
+    namespace = "fr.arrows.leaguepicker.home"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+}
