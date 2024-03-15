d9d3e8fbdf528f6492f4ad7311509f3c57be1e7e
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-15 17:34:44 +0100

    Removed unnecessary lines

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -33,11 +33,5 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
