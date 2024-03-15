0c50a723adb1d51d7914b8e137ce35eaea0b69c1
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-15 17:25:14 +0100

    Removed unnecessary lines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.application.plugin)
+    alias(libs.plugins.kotlin.android.plugin)
 }
 
 android {
@@ -26,21 +26,9 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-
+    /* Core */
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,8 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -21,4 +23,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "League Picker"
 include(":app")
- 
+include(":ui:home")
