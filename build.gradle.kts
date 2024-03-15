8507d6ef3ffa1eaeb372b5b93556cba3493d3868
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-15 17:46:47 +0100

    Modified a catalog resource name

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     alias(libs.plugins.application.plugin) apply false
     alias(libs.plugins.kotlin.android.plugin) apply false
-    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.library.plugin) apply false
 }
