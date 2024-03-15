88da4fa4461102e3e5256e8d799817af6e6b74c8
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-15 17:29:22 +0100

    Added navigation module

---
+++ navigation/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android.plugin)
+}
+
+android {
+    namespace = "fr.arrows.navigation"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 28
+
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+}
