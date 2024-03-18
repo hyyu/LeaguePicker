46c4d8dc361ee7d18138a6585b3cc20ee95a0682
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-18 21:27:01 +0100

    Collecting state on component entry

--- ui/home/src/main/java/fr/arrows/leaguepicker/home/compose/Home.kt
+++ ui/home/src/main/java/fr/arrows/leaguepicker/home/compose/Home.kt
@@ -1,8 +1,14 @@
 package fr.arrows.leaguepicker.home.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import fr.arrows.leaguepicker.home.HomeViewModel
 
 @Composable
-fun Home() {
-
+fun Home(
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+    val state by viewModel.uiState.collectAsState()
 }
