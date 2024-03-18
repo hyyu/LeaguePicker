7901962c8b745bf6edefd8e3028407938ff2be6f
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-18 19:13:24 +0100

    Added mapper for Screen class

--- navigation/src/main/java/fr/arrows/leaguepicker/navigation/Screen.kt
+++ navigation/src/main/java/fr/arrows/leaguepicker/navigation/Screen.kt
@@ -4,4 +4,12 @@ import fr.arrows.leaguepicker.navigation.values.ScreenValues
 
 sealed class Screen(val value: String) {
     data object Home : Screen(ScreenValues.HOME)
+
+    companion object {
+        fun mapValueToScreen(value: String?): Screen? = when (value) {
+            Home.value -> Home
+            else -> null
+        }
+    }
+
 }
