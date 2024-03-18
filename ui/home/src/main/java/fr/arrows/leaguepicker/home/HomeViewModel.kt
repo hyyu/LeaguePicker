e832ff53c2dc417a8ff1ba90faca91123946db2d
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-18 21:26:31 +0100

    Added stateFlows to the HomeViewModel

--- ui/home/src/main/java/fr/arrows/leaguepicker/home/HomeViewModel.kt
+++ ui/home/src/main/java/fr/arrows/leaguepicker/home/HomeViewModel.kt
@@ -1,7 +1,16 @@
 package fr.arrows.leaguepicker.home
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import fr.arrows.leaguepicker.common.viewmodel.BaseViewModel
+import fr.arrows.leaguepicker.home.state.HomeState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel {
+class HomeViewModel @Inject constructor() : BaseViewModel() {
+
+    private val _uiState = MutableStateFlow(HomeState())
+    val uiState = _uiState as StateFlow<HomeState>
+
 }
