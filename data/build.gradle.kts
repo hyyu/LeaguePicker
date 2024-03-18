ba606873fb5ff455638ada5edd3dc8abe1c66248
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-18 10:26:23 +0100

    Added Api key in data build.gradle

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library.plugin)
     alias(libs.plugins.kotlin.android.plugin)
@@ -7,10 +9,22 @@ android {
     namespace = "fr.arrows.leaguepicker.data"
     compileSdk = 34
 
+    buildFeatures.buildConfig = true
+
     defaultConfig {
         minSdk = 28
 
         consumerProguardFiles("consumer-rules.pro")
+
+        /* Api key */
+        Properties().apply {
+            if (rootProject.file("gradle.properties").exists()) {
+                load(rootProject.file("gradle.properties").inputStream())
+                getProperty("SPORTS_API_KEY")?.let {
+                    buildConfigField("String", "SPORTS_API_KEY", it)
+                }
+            }
+        }
     }
 
     buildTypes {
