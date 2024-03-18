b124aa62d77299a03b2406593d0c667a4d3f0eb3
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-18 12:02:00 +0100

    Added a start for Compose theme

---
+++ app/src/main/java/fr/arrows/leaguepicker/theme/ColorValues.kt
@@ -0,0 +1,38 @@
+package fr.arrows.leaguepicker.theme
+
+object ColorValues {
+    const val BEIGE_10 = 0xFF2A1800
+    const val BEIGE_20 = 0xFF462A00
+    const val BEIGE_30 = 0xFF643F00
+    const val BEIGE_40 = 0xFF845400
+    const val BEIGE_80 = 0xFFFFB959
+    const val BEIGE_90 = 0xFFFFDDB6
+
+    const val BROWN_10 = 0xFF380D00
+    const val BROWN_20 = 0xFF5B1B00
+    const val BROWN_30 = 0xFF7B2E0C
+    const val BROWN_40 = 0xFF9A4522
+    const val BROWN_80 = 0xFFFFB59A
+    const val BROWN_90 = 0xFFFFDBCE
+
+    const val GREEN_10 = 0xFF072100
+    const val GREEN_20 = 0xFF123800
+    const val GREEN_30 = 0xFF215106
+    const val GREEN_40 = 0xFF386A1F
+    const val GREEN_80 = 0xFF9DD67D
+    const val GREEN_90 = 0xFFB8F396
+
+    const val RED_10 = 0xFF410001
+    const val RED_20 = 0xFF690002
+    const val RED_30 = 0xFF930005
+    const val RED_40 = 0xFFBB1614
+    const val RED_80 = 0xFFFFB4A9
+    const val RED_90 = 0xFFFFDAD5
+
+    const val GRAY_06 = 0xFF16130E
+    const val GRAY_10 = 0xFF1F1B16
+    const val GRAY_90 = 0xFFEAE1D9
+    const val GRAY_98 = 0xFF16130E
+
+    const val WHITE = 0xFFFAFAFA
+}
