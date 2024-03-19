b17aee2bdbaa5720dd65383ff057d56cac4ce35f
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-19 13:08:00 +0100

    Frame class to wrap a page Scaffold

---
+++ ui/common/src/main/java/fr/arrows/leaguepicker/common/compose/frame/Frame.kt
@@ -0,0 +1,116 @@
+package fr.arrows.leaguepicker.common.compose.frame
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import fr.arrows.leaguepicker.common.compose.searchbar.SearchBarState
+import fr.arrows.leaguepicker.common.compose.snackbar.LeaguePickerSnackbar
+import fr.arrows.leaguepicker.common.model.searchbar.SearchBarItem
+import fr.arrows.leaguepicker.common.model.snackbar.LeaguepickerSnackbarData
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+@Composable
+fun Frame(
+    modifier: Modifier = Modifier,
+    snackbarState: StateFlow<LeaguepickerSnackbarData?>? = null,
+    onShowSnackbar: () -> Unit,
+    searchBarState: SearchBarState,
+    searchBarItems: List<SearchBarItem>?,
+    onSearchTextChanged: (String) -> Unit,
+    onActiveChanged: (Boolean) -> Unit,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    val showSnackbar: (LeaguepickerSnackbarData) -> Unit = {
+        coroutineScope.launch {
+            onShowSnackbar()
+            snackbarHostState.showSnackbar(
+                message = it.message,
+                actionLabel = it.type.level.name,
+                duration = SnackbarDuration.Short
+            )
+        }
+    }
+
+    snackbarState?.collectAsState()?.value.also { data ->
+        data?.let {
+            showSnackbar(it)
+        }
+    }
+
+    FrameContent(
+        modifier = modifier,
+        snackbarHostState = snackbarHostState,
+        searchBarState = searchBarState,
+        searchBarItems = searchBarItems ?: listOf(),
+        onSearchTextChanged = onSearchTextChanged,
+        onActiveChanged = onActiveChanged,
+        content = content
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun FrameContent(
+    modifier: Modifier,
+    snackbarHostState: SnackbarHostState,
+    searchBarState: SearchBarState,
+    searchBarItems: List<SearchBarItem>,
+    onSearchTextChanged: (String) -> Unit,
+    onActiveChanged: (Boolean) -> Unit,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            SearchBar(
+                query = searchBarState.text,
+                onQueryChange = onSearchTextChanged,
+                onSearch = onSearchTextChanged,
+                active = searchBarState.isSearching,
+                onActiveChange = onActiveChanged
+            ) {
+                LazyColumn {
+                    items(searchBarItems) { item ->
+                        Text(
+                            modifier = Modifier.padding(
+                                start = 8.dp,
+                                top = 4.dp,
+                                end = 8.dp,
+                                bottom = 4.dp
+                            ),
+                            text = item.text
+                        )
+                    }
+                }
+            }
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState) { data ->
+                LeaguePickerSnackbar(
+                    data = data
+                )
+            }
+        },
+        content = content
+    )
+
+}
