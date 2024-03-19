bc91c3e1ceafb24109b5b3a8923e31a17c15e4b4
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-19 19:22:30 +0100

    Modified UI for a suggestion item

--- ui/common/src/main/java/fr/arrows/leaguepicker/common/compose/searchbar/SearchResult.kt
+++ ui/common/src/main/java/fr/arrows/leaguepicker/common/compose/searchbar/SearchResult.kt
@@ -1,22 +1,16 @@
 package fr.arrows.leaguepicker.common.compose.searchbar
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun SearchResult(
+    modifier: Modifier,
     text: String
 ) {
     Text(
-        modifier = Modifier.padding(
-            start = 8.dp,
-            top = 4.dp,
-            end = 8.dp,
-            bottom = 4.dp
-        ),
+        modifier = modifier,
         text = text
     )
 }
