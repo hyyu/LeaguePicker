4677279525f997734012bbbd3b5ae88b1c02f948
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-19 23:01:37 +0100

    Added a model for teams display

---
+++ ui/common/src/main/java/fr/arrows/leaguepicker/common/model/teams/Team.kt
@@ -0,0 +1,14 @@
+package fr.arrows.leaguepicker.common.model.teams
+
+import fr.arrows.leaguepicker.domain.leagues.entity.LeagueItemEntity
+import fr.arrows.leaguepicker.domain.teams.entity.TeamItemEntity
+
+data class Team(
+    val name: String,
+    val badgeUrl: String,
+)
+
+fun TeamItemEntity.toUiModel() = Team(
+    name = name,
+    badgeUrl = badgeUrl,
+)
