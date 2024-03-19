8d9eb8511237878eefc996aedc8f60347bea69db
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-19 13:07:05 +0100

    Added events for SearchBar and first step

--- ui/home/src/main/java/fr/arrows/leaguepicker/home/event/HomeEvent.kt
+++ ui/home/src/main/java/fr/arrows/leaguepicker/home/event/HomeEvent.kt
@@ -1,4 +1,7 @@
 package fr.arrows.leaguepicker.home.event
 
 sealed class HomeEvent {
+    data class RefreshSearch(val text: String): HomeEvent()
+    data object ToggleSearch : HomeEvent()
+    data object FetchLeagues : HomeEvent()
 }
