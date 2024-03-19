a3dd184a82924f17d2b69b55b7ea0c38c440ee43
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-19 19:49:52 +0100

    Added id to the UI model

--- ui/common/src/main/java/fr/arrows/leaguepicker/common/model/leagues/League.kt
+++ ui/common/src/main/java/fr/arrows/leaguepicker/common/model/leagues/League.kt
@@ -3,12 +3,14 @@ package fr.arrows.leaguepicker.common.model.leagues
 import fr.arrows.leaguepicker.domain.repository.entity.LeagueItemEntity
 
 data class League(
+    val id: String,
     val name: String,
     val sport: String,
     val alternateNames: Sequence<String>? = null
 )
 
 fun LeagueItemEntity.toUiModel() = League(
+    id = id,
     name = officialName,
     sport = sport,
     alternateNames = alternateNames?.splitToSequence(", ")
