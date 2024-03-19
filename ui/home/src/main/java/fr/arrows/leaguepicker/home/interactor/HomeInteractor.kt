0490e67b60061bd59e94404b2809cef8b2abca70
Antoine Danibo <antoine.danibo-prestataire@laposte.fr>
2024-03-19 23:00:09 +0100

    Added a use case to HomeInteractor for teams fetching

--- ui/home/src/main/java/fr/arrows/leaguepicker/home/interactor/HomeInteractor.kt
+++ ui/home/src/main/java/fr/arrows/leaguepicker/home/interactor/HomeInteractor.kt
@@ -2,10 +2,14 @@ package fr.arrows.leaguepicker.home.interactor
 
 import fr.arrows.leaguepicker.domain.leagues.entity.FetchLeaguesEntity
 import fr.arrows.leaguepicker.domain.leagues.usecase.FetchLeagues
+import fr.arrows.leaguepicker.domain.teams.entity.FetchTeamsEntity
+import fr.arrows.leaguepicker.domain.teams.usecase.FetchTeams
 import javax.inject.Inject
 
 class HomeInteractor @Inject constructor(
-    private val fetchLeagues: FetchLeagues
+    private val fetchLeagues: FetchLeagues,
+    private val fetchTeams: FetchTeams
 ) {
     suspend fun fetchLeagues(): Result<FetchLeaguesEntity> = fetchLeagues.invoke()
+    suspend fun fetchTeams(leagueId: String): Result<FetchTeamsEntity> = fetchTeams.invoke(leagueId)
 }
